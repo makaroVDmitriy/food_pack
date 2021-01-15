666fb4008312520fced5fb3945ae528537321fcb
makaroVDmitriy <makarovdmitriy0707@gmail.com>
2021-01-16 00:23:52 +0200

    fixed content section, added slick-slider

--- #app/js/script.js
+++ #app/js/script.js
@@ -1,8 +1,39 @@
 $(function () {
+
+	/* burger */
 	let navBurger = document.querySelector('.nav__burger');
 	navBurger.addEventListener('click', function () {
 		navBurger.classList.toggle('open');
 	})
+	/* burger */
+
+	/* slider */
+	$('.slider').slick({
+		centerMode: true,
+		arrows: false,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '50px',
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerPadding: '80px',
+					slidesToShow: 1,
+				}
+			}
+		]
+	});
+	/* slider */
 
 
 });
